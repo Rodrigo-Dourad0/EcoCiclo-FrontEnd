59be3029efac5920e59417c24052e3c7c8cd7d0a
Rodrigo Almeida Dourado <115838474+Rodrigo-Dourad0@users.noreply.github.com>
2026-04-13 18:29:20 -0300

    Cria projeto
    Criação do projeto organizando a arquitetura de pastas.

---
+++ ecociclo/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', width: '100vw' }}>
+      <h1>EcoCiclo</h1>
+    </div>
+  )
+}
+
+export default App
