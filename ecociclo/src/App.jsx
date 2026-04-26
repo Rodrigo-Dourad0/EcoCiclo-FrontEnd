9a751ddb652770e0ac1f20e209dfcb0bd088583c
raibeep <20242ireads0002@ifba.edu.br>
2026-04-26 11:51:20 -0300

    fix: resolve conflito no App.jsx

--- ecociclo/src/App.jsx
+++ ecociclo/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Dashboard } from './modules/dashboard/pages/Dashboard.jsx';
 import Login from './modules/auth/pages/login.jsx';
 import CriarConta from './modules/auth/pages/CriarConta.jsx';
+import ProfilePage from './modules/profile/pages/ProfilePage.jsx';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
         <Route path="/criar-conta" element={<CriarConta />} />
+        <Route path="/perfil" element={<ProfilePage />} />
       </Routes>
     </BrowserRouter>
   );
